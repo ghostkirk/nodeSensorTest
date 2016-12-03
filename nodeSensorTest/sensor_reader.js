93706536a8f6818c9f6cd7d339687b6f693a1cca
Russell Irwin <russ@dozo.tv>
2016-12-03 15:28:34 +0000

    fixed json reader and added basic logging lib

--- nodeSensorTest/sensor_reader.js
+++ nodeSensorTest/sensor_reader.js
@@ -1,7 +1,9 @@
 ﻿'use strict'
+const util = require('util');
 
 const random = require("./local_modules/read_sensors.js");
 const rw_file = require("./local_modules/rw_file.js");
+const log = require("./local_modules/epiclog");
 
 //const writeJSON = require("./local_modules/write_json.js");
 
@@ -10,19 +12,28 @@ const low = 10;
 const high = 90;
 const configFile = "./sensor_config.json";
 
-console.log("Asking to read JSON file");
-var sensorConfigData = rw_file.readJ(configFile);
-console.log("Returned from reading file");
+log.banner("or 8");
+log.log("Asking to read JSON file");
 
+// RWI: you can read JSON files synchronously by using require
+var sensorConfigData = require(configFile);
+//var sensorConfigData = rw_file.readJ(configFile);
 
+log.info("Returned from reading file: sensorConfigData=",sensorConfigData);
+
+var giPass=0;
 function loop() {
 
     // Display some dummy sensor reading for testing.
-    console.log("Ramdom Data : " + random(low, high));
+    log.debug("Ramdom Data : " + random(low, high).toFixed(3));
 
     //Display some JSON Data for testing.
-    console.log("JSON Data = " + sensorConfigData.Sensors.Temp[1].Name);
+    log.log("JSON Data = " + sensorConfigData.Sensors.Temp[1].Name);
     setTimeout(loop, delaySeconds * 1000);
+
+    if (5 === ++giPass) {
+    	log.fatal("STOPPING COZ I FEEL LIKE IT");
+    }
 }
 
 loop();
